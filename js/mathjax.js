129f092cc0e2a1c0d874bbf1a749abb19b9f4a2e
REDDRAGON <matthias.moulin@gmail.com>
2017-08-24 12:45:33 +0200

    Update

---
+++ js/mathjax.js
@@ -0,0 +1,12 @@
+MathJax.Hub.Config({
+TeX: {
+  equationNumbers: {
+	autoNumber: "AMS"
+  },
+  extensions: ["AMScd.js"]
+},
+tex2jax: {
+  inlineMath: [ ['$','$'] ],
+  displayMath: [ ['$$','$$'] ],
+  processEscapes: true,
+}
