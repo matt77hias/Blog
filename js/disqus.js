b18c9b78d27b9832e55d87cacfde8dbbed0e5b23
REDDRAGON <matthias.moulin@gmail.com>
2017-08-24 10:45:17 +0200

    Update

---
+++ js/disqus.js
@@ -0,0 +1,6 @@
+(function() {
+    var d = document, s = d.createElement('script');
+    s.src = '//matt77hias.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
